c45a7a40af29c3e5bf9e7e81449b7c2c78a47e57
JaBUy <64704436+JaBUy@users.noreply.github.com>
2024-06-07 12:20:48 -0300

    Update ROTcipher-v2.js

--- ROTcipher-v2.js
+++ ROTcipher-v2.js
@@ -1,1 +1,26 @@
+function rot13(str) {
 
+  var cipheredStr = "";
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const key = 13;
+  
+  for (let i = 0; i < str.length; i++) {
+
+    if (/[A-Z]/.test(str[i])) {
+      let j = 0;
+      while (str[i] != alphabet[j]) {
+        j++;
+      }
+      j += key;
+      if (j >= alphabet.length) {
+        j -= alphabet.length;
+      }
+      cipheredStr += alphabet[j];
+
+    } else {
+      cipheredStr += str[i];
+    }
+  }
+  
+  return cipheredStr;
+}
